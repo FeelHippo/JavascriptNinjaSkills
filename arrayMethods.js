783ac61772bc9de314c67b6b7f80a943aa30af90
Filippo Miorin <filippomiorin@MBP-de-Mateo.home>
2021-03-13 18:32:27 +0100

    syntax + array

---
+++ arrayMethods.js
@@ -0,0 +1,84 @@
+/* ARRAY METHODS */
+
+// Array.map()
+// always prefer this over Array.forEach()
+// applies a method/function to each member of the array and RETURNS A VALUE
+
+const numbers = [1,2,3,4]
+const numbersPlusItsIndex = numbers.map((number, index) => number + index)
+console.log(numbersPlusItsIndex)
+
+// Array.forEach()
+// executes a method/function for every element of the array, DOES NOT return any value
+const letters = ['a', 'b', 'c', 'd']
+letters.forEach((letter, index) => console.log(`Letter: ${letter}, Alphabet position: ${index + 1}`))
+
+// Array.filter()
+// validates each element against the same condition, if true the element is kept, if false not kept
+const moreNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
+const evenNumbers = moreNumbers.filter(number => number % 2 == 0)
+console.log(evenNumbers)
+
+// Array.some()
+// returns 'true' if any member of the array satisfies the condition
+const instruments = ['piano', 'guitar', 'bass']
+const weHaveBass = instruments.some(instrument => instrument === 'bass')
+console.log(weHaveBass)
+
+// Array.every()
+// returns true if all elements satisfy the condition
+const randomNumbers = [...Array(5)].map(() => Math.random() * (100 - 0))
+console.log(randomNumbers.every(number => typeof number === 'number')) 
+
+// Array.reduce()
+// USE THIS TO return the sum of the elements within the array
+const numericValues = [1, 2, 3, 4, 5, 6]
+const numericReduce = numericValues.reduce((accumulator, currentArrayElement) => accumulator + currentArrayElement, 0) // the final zero is the starting point, can be omitted but is good practice
+console.log(numericReduce)
+
+const stringValues = ['Hello', 'World', 'How', 'Are', 'You?']
+const stringReduce = stringValues.reduce((accumulator, currentArrayElement) => `${accumulator} ${currentArrayElement}`)
+console.log(stringReduce)
+
+
+// JOIN ARRAYS
+const firstThree = ['a', 'b', 'c']
+const secondThree = ['d', 'e', 'f']
+
+// option 1: Array.concat()
+const result = firstThree.concat(secondThree)
+console.log(result)
+
+// option 2: spread operator
+const response = [...firstThree, ...secondThree]
+console.log(response)
+
+// SEE IF ELEMENT IS PRESENT IN AN ARRAY
+const countries = ['Italy', 'Spain', 'Germany']
+
+// option 1: Array.indexOf(). this method returns the index of the element we are looking for, or a 0 value otherwise
+console.log(countries.indexOf('Spain') > 0)
+
+// option 2: Array.includes()
+console.log(countries.includes('Lithuania'))
+
+// Array.sort()
+const randomWordsArray = ['PooPoo', 'PeePee', 'Caca', 'Filippo', 'Orlando']
+const sortedArray = randomWordsArray.sort()
+console.log(sortedArray)
+
+// PARTY TRICK .sort() takes a callback for more complex comparisons
+const items = ['réservé', 'premier', 'communiqué', 'café', 'adieu', 'éclair']
+console.log(items.sort((a, b) => a.localeCompare(b)))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ syntaxLikeAPro.js
@@ -0,0 +1,68 @@
+/* SYNTAX like a pro */
+
+// CONDITIONALS
+// use if/if else/else if you need to execute code depending on a condition:
+
+const name = 'Ringo'
+if (name === 'John' || name === 'George') {
+	console.log('guitar')
+} else if (name === 'Paul') {
+	console.log('bass')
+} else if (name === 'Ringo') {
+	console.log('drums')
+}
+
+// use a ternary operator if you have to chose between two values, depending on the truthiness of the condition
+// (condition) ? <this in case the condition is true> : <this if false>
+const numberOfString = 13
+const instrument = numberOfString === 6 ? 'guitar' : 'other instrument'
+console.log(instrument)
+
+// STRING LITERAL
+// this should be the one and only way to manipulate strings:
+// note the use of backticks ``, '' and "" will NOT work
+// within the backticks, any expression within the ${} symbol will be evaluated to string
+const band = 'beatles'
+const numberOfMembers = 4
+const didPaulDie = true
+
+const bandDescription = `The ${band.toUpperCase()} included ${didPaulDie ? numberOfMembers - 1 : numberOfMembers} original members`
+console.log('bandDescription')
+
+// STRING TO NUMBER
+const numberStringified = '5.5'
+console.log(parseInt(numberStringified)) 	// converts to an Integer
+console.log(parseFloat(numberStringified))	// converts to a Float
+console.log(Number(numberStringified))		// most commonly used
+console.log(+numberStringified)				// party trick that works incredibly well and will make all the programmers in the room think you're good at it
+
+// OPTIONAL CHAINING
+// another party trick to avoid redundant checks, VERY useful when combined with async calls to API's
+const Filippo = {}
+
+const Alberto = {
+	age: 36,
+	isYoung: false,
+	attributes: {
+		height: 195,
+		hairColor: 'blonde',
+	}
+}
+
+// without optional chaining, the below code will throw an error and stop execution of all processes
+console.log(Filippo.attributes.footSize) 
+
+// WITH optional chaining, if a property does not exist, the expression will return 'undefined', which is a common falsy value
+console.log(Alberto?.attributes?.footSize) // this translates to "does Alberto exist? OK, let's move on. Does it have a property attributes? OK, let's move on. Does it have a property footSize? NO, then return undefined"
+
+// https://javascript.info/optional-chaining
+
+
+
+
+
+
+
+
+
+
