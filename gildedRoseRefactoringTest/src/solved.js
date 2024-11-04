b549260d952e27043fa61720750eec6d5ac6f7f1
FeelHippo <mezzasegolas@gmail.com>
2024-11-04 09:46:00 +0100

    refactor

--- gildedRoseRefactoringTest/src/solved.js
+++ gildedRoseRefactoringTest/src/solved.js
@@ -26,35 +26,19 @@ class Shop {
           sellIn, 
           quality,
         }) => {
-          let updatedItem;
           switch (true) {
             case name.toLowerCase().includes(BRIE):
               // __"Aged Brie"__ actually increases in `Quality` the older it gets
               sellIn = this.updateSellIn(sellIn);
               quality = this.updateBrieQuality(quality);
-              updatedItem = {
-                name,
-                sellIn,
-                quality,
-              }
-              break;
-            case name.toLowerCase().includes(SULFURAS):
-              // __"Sulfuras"__, being a legendary item, never has to be sold or decreases in `Quality`
-              updatedItem = {
-                name,
-                sellIn,
-                quality,
-              }
               break;
             case name.toLowerCase().includes(BACKSTAGE):
               // __"Backstage passes"__, like aged brie, increases in `Quality` as its `SellIn` value approaches;
               sellIn = this.updateSellIn(sellIn);
               quality = this.updateBackstageQuality(sellIn, quality);
-              updatedItem = {
-                name,
-                sellIn,
-                quality,
-              }
+              break;
+            case name.toLowerCase().includes(SULFURAS):
+              // __"Sulfuras"__, being a legendary item, never has to be sold or decreases in `Quality`
               break;
             case name.toLowerCase().includes(CONJURED):
               // __"Conjured"__ items degrade in `Quality` twice as fast as normal items
@@ -63,24 +47,17 @@ class Shop {
                 sellIn,
                 this.updateDefaultQuality(sellIn, quality),
               );
-              updatedItem = {
-                name,
-                sellIn,
-                quality,
-              }
               break;
-          
             default:
               sellIn = this.updateSellIn(sellIn);
               quality = this.updateDefaultQuality(sellIn, quality);
-              updatedItem = {
-                name,
-                sellIn,
-                quality,
-              }
               break;
           }
-          return updatedItem;
+          return {
+            name,
+            sellIn,
+            quality,
+          }
         }
       );
   }
@@ -88,22 +65,24 @@ class Shop {
   updateSellIn(sellIn) {
     // decrease sellIn. 
     // Expiration date is 0, and cannot be negative
-    return sellIn ? sellIn - 1 : sellIn;
+    return Math.max(0, sellIn - 1);
 
   }
 
   updateDefaultQuality(sellIn, quality) {
     // Once the sell by date has passed, `Quality` degrades twice as fast
-    const updatedQuality = sellIn
-      ? quality - 1 
-      : quality - 2;
-      // The `Quality` of an item is never negative
-      return updatedQuality > 0 ? updatedQuality : 0;
+    // The `Quality` of an item is never negative
+    return Math.max(
+      0,
+      sellIn
+        ? quality - 1 
+        : quality - 2
+    );
   }
 
   updateBrieQuality(quality) {
     // The `Quality` of an item is never more than `50`
-    return quality < 50 ? quality + 1 : quality;
+    return Math.min(50, quality + 1);
   }
 
   updateBackstageQuality(sellIn, quality) {
@@ -123,7 +102,7 @@ class Shop {
       default:
         return 0;
     }
-    return updatedQuality <= 50 ? updatedQuality : 50;
+    return Math.min(50, updatedQuality);
   }
 
 }
